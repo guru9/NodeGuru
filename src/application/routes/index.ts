2b88b9ae689e7304ec0153d30f6d7ecfdbec2fb9
guru <gururaj9m@gmail.com>
2024-10-01 23:15:43 +0530

    connect to mongo database

---
+++ src/application/routes/index.ts
@@ -0,0 +1,35 @@
+﻿import { Router } from "express";
+import { userRouter } from "./user.router";
+import { bookRouter } from "./book.router";
+import { counterRouter } from "./count.router";
+
+
+const router = Router()
+
+/* 
+for all routes
+ */
+router.use('/users', userRouter)
+router.use('/books', bookRouter)
+router.use('/counts', counterRouter)
+
+
+/* 
+400- for url,method,payload not found
+*/
+router.all('*', (req, res, next) => {
+	res.status(405).json({
+		success: false,
+		message: `The request not found for url-${req.originalUrl} & type-${req.method}`,
+	})
+})
+
+
+export default router
+
+
+
+
+
+//========================8888888888888888
+
---
+++ src/core/connection/mongoDB.ts
@@ -0,0 +1,22 @@
+﻿import { connect } from "mongoose";
+import { getConfig } from "../../config"
+
+
+export const connectToDB = () => {
+
+	return new Promise<Boolean>(async (resolve, reject) => {
+		try {
+			const db = await connect(getConfig().MONGODB_URL);
+
+			if (db) {
+				console.log(`MongoDB connected to::: ${db.connection.name}`);
+				resolve(true)
+			} else {
+				resolve(false)
+			}
+		} catch (err) {
+			console.error(`DB Connection Error:-  ${err}`)
+			reject(false)
+		}
+	})
+}
